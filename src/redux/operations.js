634350027e3d2e8da502d57071c63c2f8bd900c0
Danylo Kharkiv <donatelod12@gmail.com>
2023-04-19 22:17:36 +0300

    add comp, redux

---
+++ src/redux/operations.js
@@ -0,0 +1,39 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://644033e8b9e6d064be0aa8cb.mockapi.io/";
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchContacts', async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (contact, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', contact);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteContact= createAsyncThunk(
+  'contacts/removeContact',
+  async (id, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${id}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
